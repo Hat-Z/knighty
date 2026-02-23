097640764f86bc41869b4dba0fe31cfe15e06e79
Hat-Z <vaibhavforever21@gmail.com>
2026-02-24 00:24:35 +0530

    jump and double jump

--- Scripts/player_controller.gd
+++ Scripts/player_controller.gd
@@ -1,15 +1,22 @@
 extends CharacterBody2D
 
+@export_category("movement variable")
 @export var move_speed = 120.0
 @export var decelaration = 0.1
 @export var gravity = 500.0
 @onready var sprite_2d : Sprite2D = $Sprite2D
+
+@export_category("jump variable")
+@export var jump_speed = 190.0
+@export var accelaration = 290.0
+@export var jump_amount = 2
 
 var movement = Vector2()
 
 func _physics_process(delta: float) -> void:
 	velocity.y += gravity * delta
 	horizontal_movement()
+	jump_logic()
 	set_animations()
 	flip()
 	move_and_slide()
@@ -27,6 +34,10 @@ func set_animations():
 		$anim.play("move")
 	if velocity.x == 0:
 		$anim.play("idle")
+	if velocity.y < 0:
+		$anim.play("jump")
+	if velocity.y > 10:
+		$anim.play("fall")
 
 func flip():
 	if velocity.x > 0:
@@ -35,3 +46,24 @@ func flip():
 	if velocity.x < 0:
 		sprite_2d.flip_h = true
 		#scale.x = scale.y * -1
+
+func jump_logic():
+	
+	if is_on_floor():
+		jump_amount = 2
+		if Input.is_action_just_pressed("ui_accept"):
+			jump_amount -= 1
+			velocity.y -= lerp(jump_speed, accelaration, 0.1)
+			
+	if not is_on_floor():
+		if jump_amount > 0:
+			
+			if Input.is_action_just_pressed("ui_accept"):
+				jump_amount -= 1
+				velocity.y -= lerp(jump_speed, accelaration, 1)
+				
+			if Input.is_action_just_released("ui_accept"):
+				velocity.y = lerp(velocity.y, gravity, 0.2)
+				velocity.y *= 0.3
+	else:
+		return
