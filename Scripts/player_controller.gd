8488e045aab42b3b2e4909430ec812ee7b108f1c
Hat-Z <vaibhavforever21@gmail.com>
2026-02-24 17:46:04 +0530

    wall jump

--- Scripts/player_controller.gd
+++ Scripts/player_controller.gd
@@ -11,23 +11,33 @@ extends CharacterBody2D
 @export var accelaration = 290.0
 @export var jump_amount = 2
 
+@export_category("wall variable")
+@export var wall_slide = 50.0
+@onready var raycast_left: RayCast2D = $Node2D/raycast_left
+@onready var raycast_right: RayCast2D = $Node2D/raycast_right
+@export var wall_x_force = 200.0
+@export var wall_y_force = -220.0
+var is_wall_jumping = false
+
 var movement = Vector2()
 
 func _physics_process(delta: float) -> void:
 	velocity.y += gravity * delta
 	horizontal_movement()
 	jump_logic()
+	wall_logic()
 	set_animations()
 	flip()
 	move_and_slide()
 
 func horizontal_movement():
-	movement = Input.get_axis("ui_left","ui_right")
-	
-	if movement:
-		velocity.x = movement * move_speed
-	else:
-		velocity.x = move_toward(velocity.x, 0, move_speed * decelaration)
+	if is_wall_jumping == false:
+		movement = Input.get_axis("ui_left","ui_right")
+		
+		if movement:
+			velocity.x = movement * move_speed
+		else:
+			velocity.x = move_toward(velocity.x, 0, move_speed * decelaration)
 		
 func set_animations():
 	if velocity.x != 0:
@@ -38,14 +48,18 @@ func set_animations():
 		$anim.play("jump")
 	if velocity.y > 10:
 		$anim.play("fall")
+	if is_on_wall_only():
+		$anim.play("fall")
 
 func flip():
 	if velocity.x > 0:
 		sprite_2d.flip_h = false
 		#scale.x = scale.y * 1
+		#wall_x_force = 200
 	if velocity.x < 0:
 		sprite_2d.flip_h = true
 		#scale.x = scale.y * -1
+		#wall_x_force = -200
 
 func jump_logic():
 	
@@ -67,3 +81,21 @@ func jump_logic():
 				velocity.y *= 0.3
 	else:
 		return
+		
+func wall_logic():
+	if is_on_wall_only():
+		velocity.y = wall_slide
+		if Input.is_action_just_pressed("ui_accept"):
+			if raycast_left.is_colliding():
+				jump_amount = 2
+				velocity = Vector2(wall_x_force, wall_y_force)
+				wall_jumping()
+			if raycast_right.is_colliding():
+				jump_amount = 2
+				velocity = Vector2(-wall_x_force, wall_y_force)
+				wall_jumping()
+
+func wall_jumping():
+	is_wall_jumping = true
+	await get_tree().create_timer(0.12).timeout
+	is_wall_jumping = false
