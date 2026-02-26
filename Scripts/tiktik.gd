9cce97c9bc2ab07b46f18f84d7d983c29623bfcb
Hat-Z <vaibhavforever21@gmail.com>
2026-02-26 18:49:47 +0530

    health bar

--- Scripts/tiktik.gd
+++ Scripts/tiktik.gd
@@ -1,6 +1,7 @@
 extends CharacterBody2D
 
 @export var health : int = 2
+@export var damage = 1.0
 
 func _process(delta: float) -> void:
 	if health <= 0:
@@ -9,3 +10,8 @@ func _process(delta: float) -> void:
 func _on_hitbox_area_entered(area: Area2D) -> void:
 	if area.is_in_group("sword"):
 		health -= 1
+
+
+func _on_hitbox_body_entered(body: Node2D) -> void:
+	if body.is_in_group("player"):
+		Global.health -= damage
